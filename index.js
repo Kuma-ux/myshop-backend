3ecdc0fb0076e3ee045a9674f080a1296803eef5
Kuma-ux <mollyjane797@gmail.com>
2026-05-07 16:12:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -94,6 +94,6 @@ app.delete("/api/products/:id", auth, async (req, res) => {
 });
 /* ---------------- START SERVER ---------------- */
 const PORT = process.env.PORT || 5000;
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
